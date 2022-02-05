97ee7dbb40f237cb05ab7d74e15b4be8babc7e78
Aditya <akaditya394@gmail.com>
2022-02-05 12:14:49 +0530

    basic structure added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import styles from "./App.module.css";
 import AddNote from "../src/components/AddNote/AddNote";
+import Note from "../src/components/Note/Note"
 
 const App = () => {
   return (
-    <div>
+    <div className={styles.flexbox}>
       <AddNote />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
     </div>
   );
 };
