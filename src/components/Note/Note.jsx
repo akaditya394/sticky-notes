8e2ee96ff8685ee3312e96c5c2f8328712fad0fb
Aditya <akaditya394@gmail.com>
2022-02-05 12:04:05 +0530

    Note component added

---
+++ src/components/Note/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Note = () => {
+  return (
+    <div>
+      <div>
+        <h3>Note Title</h3>
+        <p> Note Text</p>
+      </div>
+    </div>
+  );
+};
+
+export default Note;
+f;
